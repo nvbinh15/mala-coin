7c7b5a3b4a82cc3f73c23a34aa7105417b13f65a
nvbinh15 <binh.pdc01@gmail.com>
2021-07-02 20:57:34 +0800

    Update block and blockchain class

--- malacoin/tests.py
+++ malacoin/tests.py
@@ -11,9 +11,12 @@ chain.add_block(block1)
 
 transaction2 = Transaction('c', 'd', 50)
 transaction3 = Transaction('a', 'd', 150)
-block2 = Block(transactions=[transaction2, transaction3], time='tomorrow', index=2)
+transaction4 = Transaction('a', 'b', 10000000)
+block2 = Block(transactions=[transaction2, transaction3, transaction4], time='tomorrow', index=2)
 block2.mine_block()
 chain.add_block(block2)
+
+chain.generate_keys()
 
 for block in chain.chain:
     print(block)
